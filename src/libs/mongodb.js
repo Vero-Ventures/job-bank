040a38d845a7a18a100d4630a5a0fd217e322a15
amamc <ama.m.cantabile@gmail.com>
2024-05-04 15:55:01 -0700

    Added mongoDB connection configuration

---
+++ src/libs/mongodb.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export const connectMongoDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_CONNECT);
+        console.log('Connected to MongoDB');
+
+    } catch (error) {
+        console.log('Error connecting to MongoDB', error);
+    }
+};
