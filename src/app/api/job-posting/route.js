c15fa28a439990b9ca9e9fc3a1e9284a6d47661f
amadeus <ama.m.cantabile@gmail.com>
2024-05-04 18:42:03 -0700

    Added delete request that handles deleting a job posting and removed unnecessary comment and console.log codes

--- src/app/api/job-posting/route.js
+++ src/app/api/job-posting/route.js
@@ -5,11 +5,8 @@ import mongoose from 'mongoose';
 
 export async function GET(req) {
     try {
-        // Extract email parameter from the request query
         const email = req.nextUrl.searchParams.get('email');
-        console.log(email)
 
-        // Check if email parameter is provided
         if (!email) {
             return NextResponse.json({ message: 'Bad Request - Email parameter is required' }, { status: 400 });
         }
@@ -18,14 +15,12 @@ export async function GET(req) {
 
         const Posting = mongoose.models.posting || mongoose.model('posting', posting);
 
-        // Query the database to find job postings associated with the provided email
         const jobPostings = await Posting.find({ email });
 
         await mongoose.connection.close();
 
         // Check if job postings were found
         if (jobPostings.length === 0) {
-            // No job postings were found associated with the provided email
             return NextResponse.json({ message: 'Not Found - No job postings were found associated with the provided email' }, { status: 404 });
         }
 
@@ -57,9 +52,9 @@ export async function GET(req) {
     }
 }
 
-export async function POST(request) {
+export async function POST(req) {
     try {
-        const jobPosting = await request.json();
+        const jobPosting = await req.json();
 
         await connectMongoDB();
 
@@ -80,6 +75,53 @@ export async function POST(request) {
 
     } catch (error) {
         console.log('Error creating job postings', error);
+
+        await mongoose.connection.close();
+
+        // Check error status and return appropriate response
+        if (error instanceof SyntaxError || error instanceof TypeError) {
+            // Malformed or invalid request
+            return NextResponse.json({ message: 'Bad Request - The request is malformed or invalid' }, { status: 400 });
+
+        } else if (error.name === 'UnauthorizedError') {
+            // Unauthorized
+            return NextResponse.json({ message: 'Unauthorized - The client is not authorized to perform the operation' }, { status: 401 });
+
+        } else if (error.name === 'NotFoundError') {
+            // Not Found
+            return NextResponse.json({ message: 'Not Found - The specified job ID does not exist' }, { status: 404 });
+
+        } else {
+            // Other server error
+            return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
+        }
+    }
+}
+
+export async function DELETE(req) {
+    try {
+        const jobPostingId = req.nextUrl.searchParams.get('job-posting-id');
+
+        if (!jobPostingId) {
+            return NextResponse.json({ message: 'Bad Request - Job posting ID is required' }, { status: 400 });
+        }
+
+        await connectMongoDB();
+
+        const Posting = mongoose.models.posting || mongoose.model('posting', posting);
+
+        const deletePosting = await Posting.findOneAndDelete({ _id: jobPostingId });
+
+        if (!deletePosting) {
+            return NextResponse.json({ message: 'Not Found - Job posting with the specified ID does not exist' }, { status: 404 });
+        }
+
+        await mongoose.connection.close();
+
+        return NextResponse.json({ message: 'Job postings deleted successfully' }, { status: 200 });
+
+    } catch (error) {
+        console.log('Error deleting job postings', error);
 
         await mongoose.connection.close();
 
