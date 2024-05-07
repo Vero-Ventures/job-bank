338e7e694d34e45656a41959f0a26b93ce1535a1
amamc <ama.m.cantabile@gmail.com>
2024-05-07 10:11:42 -0700

    Added pagination feature to the get request in site1,2,3, and 4

--- src/app/api/job-posting/site-2/route.js
+++ src/app/api/job-posting/site-2/route.js
@@ -3,21 +3,34 @@ import { connectMongoDB } from '@/libs/mongodb';
 import posting from '@/app/api/posting';
 import mongoose from 'mongoose';
 
-export async function GET() {
+export async function GET(req) {
     try {
+        const pageSize = 25;
+        const pageNum = parseInt(req.nextUrl.searchParams.get('page_num'));
+        const skip = (pageNum - 1) * pageSize;
+
+        if (isNaN(pageNum) || pageNum < 1) {
+            return NextResponse.json({ message: 'Bad Request - Invalid page number' }, { status: 400 });
+        }
+
         await connectMongoDB();
 
         const Posting = mongoose.models.posting || mongoose.model('posting', posting);
 
-        const jobPostings = await Posting.find({ site2: true });
+        // Query job postings with pagination
+        const jobPostings = await Posting.find({ site2: true }).skip(skip).limit(pageSize);
 
         await mongoose.connection.close();
+
+        if (jobPostings.length === 0) {
+            return NextResponse.json({ message: 'Not Found - No job postings found on this page' }, { status: 404 });
+        }
 
         return NextResponse.json({ jobPostings }, { status: 200 });
 
     } catch (error) {
 
-        console.log('Error fetching job postings by email:', error);
+        console.log('Error fetching job postings:', error);
 
         await mongoose.connection.close();
 
