e40065da6836d5692a4962943aefb4a9cf614d98
Amadeus Min <60831316+ama-cantabile@users.noreply.github.com>
2024-05-10 15:31:11 -0700

    [Issue # 17] Job Posting CRUD API for Backend Service  (#19)
    * Added mongoDB connection configuration
    
    * Added a post request for job posting
    
    * Added get request to search job postings by email in route.js
    
    * Added delete request that handles deleting a job posting and removed unnecessary comment and console.log codes
    
    * Added Patch request which updates any desired fields in the job posting colletion in route.js
    
    * Removed unused import and removed unnecessary space
    
    * Added get requests for site 1, 2, 3, and 4
    
    * Added pagination feature to the get request in site1,2,3, and 4
    
    * Created siteRequestUtils.js and moved the repeated codes in sites' route codes to it. Also, updated the site names specifically.
    
    * Updated the database connection to handle it in singleton way
    
    * Removed database connection closing code to fix the connection error and added missing import NextResponse in siteRequestUtils
    
    * Removed unnecessary error handling and fixed typo
    
    * Added get request by Id for job posting
    
    * Apply formatting changes
    
    ---------
    
    Co-authored-by: ama-cantabile <ama-cantabile@users.noreply.github.com>

---
+++ src/app/api/job-posting/newcomers/route.js
@@ -0,0 +1,32 @@
+import { NextResponse } from 'next/server';
+import {
+  getPaginationParams,
+  fetchJobPostings,
+  handleError,
+} from '../siteRequestUtils';
+
+export async function GET(req) {
+  try {
+    //Todo: Update the site2 name to newcomers
+    const siteCriteria = { site2: true };
+
+    // Extract pagination parameters
+    const { skip, pageSize } = getPaginationParams(req);
+
+    // Query job postings with pagination
+    const jobPostings = await fetchJobPostings(siteCriteria, skip, pageSize);
+
+    if (jobPostings.length === 0) {
+      return NextResponse.json(
+        { message: 'Not Found - No job postings found on this page' },
+        { status: 404 }
+      );
+    }
+
+    // Return success response with the paginated job postings
+    return NextResponse.json({ jobPostings }, { status: 200 });
+  } catch (error) {
+    // Handle errors
+    return handleError(error);
+  }
+}
