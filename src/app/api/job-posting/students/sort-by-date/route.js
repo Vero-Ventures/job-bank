a3df5038d4d0ad945e567334922fdd36a6e522a4
amamc <ama.m.cantabile@gmail.com>
2024-05-11 22:02:28 -0700

    Added Get request that returns sorted job posting by date for students front-end

---
+++ src/app/api/job-posting/students/sort-by-date/route.js
@@ -0,0 +1,36 @@
+import { NextResponse } from 'next/server';
+import {
+  getPaginationParams,
+  fetchSortedJobPostingsByDate,
+  handleError,
+} from '../../siteRequestUtils';
+
+export async function GET(req) {
+  try {
+    //Todo: Update the site4 name to students
+    const siteCriteria = { site4: true };
+
+    // Extract pagination parameters
+    const { skip, pageSize } = getPaginationParams(req);
+
+    // Query sorted job postings by posting date with pagination
+    const jobPostings = await fetchSortedJobPostingsByDate(
+      siteCriteria,
+      skip,
+      pageSize
+    );
+
+    if (jobPostings.length < 1) {
+      return NextResponse.json(
+        { message: 'Not Found - No job postings found on this page' },
+        { status: 404 }
+      );
+    }
+
+    // Return success response with the paginated job postings
+    return NextResponse.json({ jobPostings }, { status: 200 });
+  } catch (error) {
+    // Handle errors
+    return handleError(error);
+  }
+}
