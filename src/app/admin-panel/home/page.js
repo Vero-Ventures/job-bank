692cce83c2d9fb9191d6f5e9c9a587a8fc0c0276
shye <113232835+justshye@users.noreply.github.com>
2024-05-17 15:14:20 -0700

    Add margin to top of cards

--- src/app/admin-panel/home/page.js
+++ src/app/admin-panel/home/page.js
@@ -147,7 +147,7 @@ export default function Home() {
           </div>
         </div>
       )}
-      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-4">
+      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-4 mt-5">
         {loading ? (
           <p>Loading...</p>
         ) : jobPostings.length > 0 ? (
