3a36682204a7f9686d392c73848d6aa283d30ee8
mai-vu <vhmai3007@gmail.com>
2024-05-16 22:56:33 -0700

    added sent field

--- src/components/ui/addJobPostingForm.jsx
+++ src/components/ui/addJobPostingForm.jsx
@@ -24,6 +24,7 @@ const AddJobPostingForm = ({ onSubmit, email }) => {
     site3: false,
     site4: false,
     site5: false,
+    sent: true,
   });
 
   const sites = ['Indigenous', 'New Comers', 'Site 3', 'Site 4', 'Site 5'];
