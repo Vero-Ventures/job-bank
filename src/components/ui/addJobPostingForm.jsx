a52e83676b2df182d164e094f6f849da233e514b
mai-vu <vhmai3007@gmail.com>
2024-05-17 12:39:11 -0700

    Added Language field

--- src/components/ui/addJobPostingForm.jsx
+++ src/components/ui/addJobPostingForm.jsx
@@ -10,6 +10,7 @@ const AddJobPostingForm = ({ onSubmit, email }) => {
     streetAddress: '',
     addressLocality: '',
     addressRegion: '',
+    language: '',
     employmentType: '',
     employmentSubType: '',
     minCompValue: '',
@@ -141,7 +142,22 @@ const AddJobPostingForm = ({ onSubmit, email }) => {
               </select>
             </div>
           </div>
-
+          <div>
+            <label className="block text-sm font-semibold leading-6 text-gray-900">
+              Language
+            </label>
+            <div className="mt-2.5">
+              <input
+                type="text"
+                name="language"
+                value={formData.language}
+                onChange={handleChange}
+                placeholder="Language"
+                required
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
           <div className="sm:col-span-2">
             <label className="block text-sm font-semibold leading-6 text-gray-900">
               Employment Type
