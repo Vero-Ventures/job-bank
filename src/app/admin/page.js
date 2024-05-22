bf86f7717d96c44a272bf0d6b0b4a2c6ec4c19ee
mai-vu <vhmai3007@gmail.com>
2024-05-21 18:42:13 -0700

    migrated code to emailHandler

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -1,82 +1,40 @@
 'use client';
 
 import React, { useState, useEffect } from 'react';
+import emailHandler from './emailHandler';
 import { AdminPage } from '@/components/component/adminPage';
 
 export default function Home() {
-  const JOB_POSTING_API_URL = process.env.NEXT_PUBLIC_JOB_POSTING_API_URL;
   const [emails, setEmails] = useState([]);
 
   // Function to fetch emails from an API endpoint
   const fetchEmails = async () => {
     try {
-      const response = await fetch(`${JOB_POSTING_API_URL}email-sent?sort=-1`);
-      const data = await response.json();
-      setEmails(data.emailAddresses);
+      const data = await emailHandler.getContactStat();
+      setEmails(data);
     } catch (error) {
       console.error('Error fetching emails:', error);
-      return [];
     }
   };
 
-  const updateSentStatus = async (email, sent) => {
-    try {
-      const apiUrl = `${JOB_POSTING_API_URL}/email-sent/`;
-      const response = await fetch(apiUrl, {
-        method: 'PATCH',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email, sent }),
-      });
-
-      if (response.ok) {
-        // Update the status of the email in the data array
-        setEmails(prevEmails =>
-          prevEmails.map(emailObj =>
-            emailObj.email === email ? { ...emailObj, sent } : emailObj
-          )
-        );
-
-        const data = await response.json();
-        console.log(data.message);
-      } else {
-        throw new Error('Failed to update sent status');
-      }
-    } catch (error) {
-      console.error('Error updating sent status:', error);
-    }
-  };
-
-  // Fetch emails when component mounts
   useEffect(() => {
     fetchEmails();
   }, []);
 
   const sendEmail = async recipient => {
     try {
-      const apiUrl = process.env.NEXT_PUBLIC_SEND_EMAIL_API_URL;
-      const response = await fetch(apiUrl, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          to: recipient,
-          subject: 'Videre Financiers - Job Bank Platform',
-          text: 'and easy to do anywhere, even with Node.js',
-          html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-        }),
-      });
-
-      if (response.ok) {
-        updateSentStatus(recipient, true);
-      } else {
-        throw new Error('Failed to send email');
-      }
+      await emailHandler.sendEmail(recipient);
     } catch (error) {
       console.error('Error sending email:', error);
     }
+
+    const sent = true;
+    // Update the status of the email in the emails state
+    setEmails(prevEmails =>
+      prevEmails.map(emailObj =>
+        emailObj.email === recipient ? { ...emailObj, sent } : emailObj
+      )
+    );
   };
 
   return <AdminPage data={emails} sendEmail={sendEmail} />;
