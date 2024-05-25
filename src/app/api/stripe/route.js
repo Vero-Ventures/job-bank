d6ae241cf0351a4bb165fe4bbeb18d6ec94e8d12
shye <113232835+justshye@users.noreply.github.com>
2024-05-25 04:30:10 -0700

    Update to NEXT_PUBLIC env

--- src/app/api/stripe/route.js
+++ src/app/api/stripe/route.js
@@ -1,7 +1,9 @@
 import { NextResponse } from 'next/server';
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe')(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
 
 export async function POST(req) {
+  // const { jobPostingsLength } = await req.json(); // Extract jobPostingsLength from the request body
+
   const baseURL = req.headers.get('referer');
 
   try {
@@ -10,8 +12,8 @@ export async function POST(req) {
       line_items: [
         {
           // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-          price: process.env.STRIPE_JOB_POSTING_PRICE_ID,
-          quantity: 1,
+          price: process.env.NEXT_PUBLIC_STRIPE_JOB_POSTING_PRICE_ID,
+          quantity: 2,
         },
       ],
       mode: 'payment',
