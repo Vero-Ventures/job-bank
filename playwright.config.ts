381d191cb68bfef65ab32cc5ce410a7b38051a39
Cassidy Boilley <cboilley@my.bcit.ca>
2024-05-25 21:42:14 -0700

    trying a fix for api tests in GH actions

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,12 +25,12 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 
-  // webServer: {
-  //   command: 'npm run dev',
-  //   url: 'http://127.0.0.1:3000/',
-  //   timeout: 120 * 1000,
-  //   reuseExistingServer: true,
-  // },
+  webServer: {
+    command: 'npm run dev',
+    url: 'http://127.0.0.1:3000/',
+    timeout: 120 * 1000,
+    reuseExistingServer: true,
+  },
   use: {
     trace: 'on-first-retry',
   },
@@ -42,15 +42,15 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
