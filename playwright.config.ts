2a833aa3cc59f9db0fec3b46e543d65e0a4c4557
Cassidy Boilley <cboilley@my.bcit.ca>
2024-05-25 21:21:27 -0700

    trying a fix for api tests in GH actions

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
     command: 'npm run dev',
     url: 'http://127.0.0.1:3000/',
     timeout: 120 * 1000,
-    reuseExistingServer: !process.env.CI,
+    reuseExistingServer: true,
   },
   use: {
     trace: 'on-first-retry',
