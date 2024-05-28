c6db2029ad5759d472aac7c5df2b3f3f78408801
mai-vu <vhmai3007@gmail.com>
2024-05-27 17:30:09 -0700

    aded posting status in Edit view

--- src/app/admin-panel/postingDetails/page.js
+++ src/app/admin-panel/postingDetails/page.js
@@ -84,6 +84,16 @@ export default function DetailsPage() {
   return (
     <div className="isolate bg-white px-2 py-2 sm:py-2 lg:px-2">
       <form className="mx-auto mt-16 max-w-4xl sm:mt-2 ">
+        <div className="flex items-center">
+          <label className="text-sm font-semibold leading-6 text-gray-900 mr-2">
+            Posting Status:
+          </label>
+          <span
+            className={`text-sm font-semibold ${jobPosting.paid ? 'text-green-500' : 'text-yellow-500'}`}>
+            {jobPosting.paid ? 'Active' : 'Pending'}
+          </span>
+        </div>
+
         <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
           <div>
             <label className="block text-sm font-semibold leading-6 text-gray-900">
