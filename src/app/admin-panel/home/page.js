671e82cf2fc789be84b3d296abf06779f69a449f
mai-vu <vhmai3007@gmail.com>
2024-05-28 14:13:18 -0700

    deleted unussed lines

--- src/app/admin-panel/home/page.js
+++ src/app/admin-panel/home/page.js
@@ -190,9 +190,6 @@ export default function Home() {
               email={user.email}
               onClose={() => setShowForm(false)}
             />
-            {/* <button
-              className="close-icon absolute top-0 right-0 p-2 text-gray-500 hover:text-gray-700"
-              onClick={() => setShowForm(false)}></button> */}
           </div>
         </div>
       )}
